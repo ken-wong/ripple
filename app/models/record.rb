9fef395d0ea4e23d04ceb71f0d65be8ce47f5786
ken-wong <wangkun025@gmail.com>
2017-01-10 15:17:56 +0800

    Add summary

--- app/models/record.rb
+++ app/models/record.rb
@@ -3,6 +3,6 @@ class Record < ActiveRecord::Base
   belongs_to :project
 
   validates :user_id, presence: true
-  validates :project_id, presence: true
+  validates :project_id, presence: true, :uniqueness => {:scope => [:user_id, :date]}
   validates :date, presence: true
 end
